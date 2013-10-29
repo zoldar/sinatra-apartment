2aac1eab5c00f629a6c4f1ad016988247c5bd7da
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-10-29 15:23:42 +0100

    Changed model creation in tests to a version throwing exceptions

--- test/landlord/schedule_entry_test.rb
+++ test/landlord/schedule_entry_test.rb
@@ -4,30 +4,23 @@ require './test/test_helper'
 class ScheduleEntryTest < Minitest::Test
   def setup
     super
-    @apartment = Model::Apartment.create(name: "Apartment 3")
-    @apartment.save
+    @apartment = Model::Apartment.create!(name: "Apartment 3")
   end
   
-  def teardown
-    super
-    @apartment = nil
-  end
-
   def test_valid_entry_creation
-    s = Model::ScheduleEntry.create(apartment_id: @apartment.id, 
-                                    from: Date.new(2013, 2, 13),
-                                    to: Date.new(2013, 2, 18),
-                                    state: 'available')
-    assert_equal true, s.save
+    Model::ScheduleEntry.create!(apartment_id: @apartment.id, 
+                                 from: Date.new(2013, 2, 13),
+                                 to: Date.new(2013, 2, 18),
+                                 state: 'available')
   end
   
   def test_to_after_from_should_fail
-    s = Model::ScheduleEntry.create(apartment_id: @apartment.id,
-                                    from: Date.new(2013, 2, 18),
-                                    to: Date.new(2013, 2, 13),
-                                    state: 'available')
-
-    assert_equal false, s.save
+    assert_raises(ActiveRecord::RecordInvalid) do
+      Model::ScheduleEntry.create!(apartment_id: @apartment.id,
+                                   from: Date.new(2013, 2, 18),
+                                   to: Date.new(2013, 2, 13),
+                                   state: 'available')
+    end
   end
 end
  
