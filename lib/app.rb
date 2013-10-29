302eeabeb381300235137f039450cf42fff85835
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-10-29 15:25:19 +0100

    Removed redundant helper. request.base_url can be used instead.

--- lib/app.rb
+++ lib/app.rb
@@ -10,15 +10,4 @@ class LandLordApp < Sinatra::Base
   enable :sessions
   set :session_secret, ENV.fetch('SESSION_SECRET') { "Need to know only." }
   use Rack::Flash
-
-  helpers do
-
-    def site_root
-      if ENV['RACK_ENV'].to_sym == :production
-        ENV.fetch('SITE_ROOT') { "http://apartment.zoldar.net" }
-      else
-        'http://localhost:4567'
-      end
-    end
-  end
 end
