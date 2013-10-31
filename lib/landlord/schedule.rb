66704c90516f52d435d794e3844aad5d9d2131e9
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-10-31 01:19:50 +0100

    Working logic for availability check of schedule.

--- lib/landlord/schedule.rb
+++ lib/landlord/schedule.rb
@@ -11,8 +11,44 @@ module Model
     
     def available?(from, to)
       raise ArgumentError, "To cannot be the same or earlier than from." unless from <= to
-      
-      @apartment.schedule_entry.where("`from` <= ? and `to` >= ?", from, to).count > 0
+
+      entries_available = entries_overlapping_with_range('available', from, to)
+      entries_unavailable = entries_overlapping_with_range('unavailable', from, to)
+
+      contained_in_available_entries?(entries_available, from, to) &&
+        !entries_unavailable.any?
+    end
+    
+    private
+    
+    def contained_in_available_entries?(entries, from, to)
+      entries.any? && 
+        entries_contain_range?(entries, from, to) && 
+        continuous?(entries)
+    end
+
+    def entries_overlapping_with_range(state, from, to)
+      @apartment.schedule_entry.where("`from` <= ? and `to` >= ? and state = ?", 
+                                      to, from, state).order("`from`")
+    end
+    
+    def entries_contain_range?(entries, from, to)
+      entries.first.from <= from && entries.last.to >= to
+    end
+    
+    def continuous?(entries)
+      continuity_flag = true
+      previous_entry = nil
+      # FIXME: Wanted to use find_each but in ends up with a weird exception.
+      # Needs consulting.
+      entries.each do |entry|
+        if !previous_entry.nil? && entry.from > previous_entry.to
+          continuity_flag = false
+          break
+        end
+        previous_entry = entry
+      end
+      continuity_flag
     end
   end
 end
--- test/landlord/schedule_test.rb
+++ test/landlord/schedule_test.rb
@@ -85,5 +85,139 @@ class ScheduleWithTwoContinuousEntriesTest < Minitest::Test
 
     @schedule = Model::Schedule.for(@apartment)
   end
+  
+  def test_available_when_spans_multiple_entries
+    assert_equal true, @schedule.available?(Date.new(2013, 2, 15),
+                                            Date.new(2013, 2, 21))
+  end
+  
+  def test_unavailable_when_spans_multiple_and_over_right_edge
+    assert_equal false, @schedule.available?(Date.new(2013, 2, 15),
+                                             Date.new(2013, 2, 26))
+  end
+
+  def test_unavailable_when_spans_multiple_and_over_left_edge
+    assert_equal false, @schedule.available?(Date.new(2013, 2, 11),
+                                             Date.new(2013, 2, 23))
+  end
+
+  def test_unavailable_when_spans_multiple_and_over_both_edges
+    assert_equal false, @schedule.available?(Date.new(2013, 2, 12),
+                                             Date.new(2013, 2, 26))
+  end
+
+  def test_available_when_contained_in_a_single_entry
+    assert_equal true, @schedule.available?(Date.new(2013, 2, 14),
+                                            Date.new(2013, 2, 16))
+  end
+  
+  def test_unavailable_when_completely_outside_entries
+    assert_equal false, @schedule.available?(Date.new(2013, 5, 12),
+                                             Date.new(2013, 5, 26))
+  end
+  
+  def test_available_when_covers_the_whole_range_of_entries
+    assert_equal false, @schedule.available?(Date.new(2013, 5, 13),
+                                             Date.new(2013, 5, 24))
+  end
 end
  
+class ScheduleWithTwoIntermittentEntriesTest < Minitest::Test
+  def setup
+    super
+
+    @apartment = Model::Apartment.create!(name: "Apartment with schedule")
+    
+    Model::ScheduleEntry.create!(apartment_id: @apartment.id, 
+                                 from: Date.new(2013, 2, 13),
+                                 to: Date.new(2013, 2, 18),
+                                 state: 'available')
+
+    Model::ScheduleEntry.create!(apartment_id: @apartment.id, 
+                                 from: Date.new(2013, 2, 20),
+                                 to: Date.new(2013, 2, 24),
+                                 state: 'available')
+
+    @schedule = Model::Schedule.for(@apartment)
+  end
+
+  def test_unavailable_when_spans_multiple_entries
+    assert_equal false, @schedule.available?(Date.new(2013, 2, 15),
+                                             Date.new(2013, 2, 21))
+  end
+  
+  def test_available_when_contained_in_a_single_entry
+    assert_equal true, @schedule.available?(Date.new(2013, 2, 21),
+                                            Date.new(2013, 2, 23))
+  end
+end 
+
+class ScheduleWithTwoAvailableOverlappingEntries < Minitest::Test
+   def setup
+    super
+
+    @apartment = Model::Apartment.create!(name: "Apartment with schedule")
+    
+    Model::ScheduleEntry.create!(apartment_id: @apartment.id, 
+                                 from: Date.new(2013, 2, 13),
+                                 to: Date.new(2013, 2, 24),
+                                 state: 'available')
+
+    Model::ScheduleEntry.create!(apartment_id: @apartment.id, 
+                                 from: Date.new(2013, 2, 15),
+                                 to: Date.new(2013, 2, 20),
+                                 state: 'available')
+
+    @schedule = Model::Schedule.for(@apartment)
+  end
+
+  def test_available_when_contained_in_both_entries
+    assert_equal true, @schedule.available?(Date.new(2013, 2, 16),
+                                            Date.new(2013, 2, 18))
+  end
+ 
+  def test_available_when_contained_in_a_single_entry
+    assert_equal true, @schedule.available?(Date.new(2013, 2, 21),
+                                            Date.new(2013, 2, 23))
+  end
+end
+
+class ScheduleWithTwoAlternateOverlappingEntries < Minitest::Test
+   def setup
+    super
+
+    @apartment = Model::Apartment.create!(name: "Apartment with schedule")
+    
+    Model::ScheduleEntry.create!(apartment_id: @apartment.id, 
+                                 from: Date.new(2013, 2, 13),
+                                 to: Date.new(2013, 2, 24),
+                                 state: 'available')
+
+    Model::ScheduleEntry.create!(apartment_id: @apartment.id, 
+                                 from: Date.new(2013, 2, 15),
+                                 to: Date.new(2013, 2, 20),
+                                 state: 'unavailable')
+
+    @schedule = Model::Schedule.for(@apartment)
+  end
+
+  def test_unavailable_when_contained_in_both_entries
+    assert_equal false, @schedule.available?(Date.new(2013, 2, 16),
+                                             Date.new(2013, 2, 18))
+  end
+
+  def test_unavailable_when_over_the_edge_of_inner_entry
+    assert_equal false, @schedule.available?(Date.new(2013, 2, 14),
+                                             Date.new(2013, 2, 18))
+  end
+
+  def test_unavailable_when_over_the_edge_of_both_entries
+    assert_equal false, @schedule.available?(Date.new(2013, 2, 14),
+                                             Date.new(2013, 2, 26))
+  end
+  
+  def test_available_when_contained_only_in_outer_entry
+    assert_equal true, @schedule.available?(Date.new(2013, 2, 21),
+                                            Date.new(2013, 2, 23))
+  end
+end
