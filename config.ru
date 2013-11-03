18250d2136f61a384fe0ae34200d9bdd3be38d1c
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-11-03 11:46:46 +0100

    Added API application

--- config.ru
+++ config.ru
@@ -4,8 +4,13 @@ require 'bundler'
 Bundler.require
 
 require 'app'
+require 'api'
 
 ENV['RACK_ENV'] ||= 'development'
 
 use ActiveRecord::ConnectionAdapters::ConnectionManagement
 run ApartmentApp
+
+map '/api/v1/' do
+  run LandLordAPI
+end
---
+++ lib/api.rb
@@ -0,0 +1,12 @@
+require 'landlord'
+require 'sinatra/petroglyph'
+
+class LandLordAPI < Sinatra::Base
+  set :environment, ENV.fetch('RACK_ENV') { :development }.to_sym
+  set :root, 'lib/api'
+  set :method_override, true
+
+  enable :sessions
+  set :session_secret, ENV.fetch('SESSION_SECRET') { "Need to know only." }
+  use Rack::Flash
+end
