b99140e8664530186a0816e802fe65e08e0c6472
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-11-03 11:40:02 +0100

    Added environment declaration.

--- lib/app.rb
+++ lib/app.rb
@@ -3,7 +3,7 @@ require 'landlord'
 require 'app/front'
 
 class LandLordApp < Sinatra::Base
-
+  set :environment, ENV.fetch('RACK_ENV') { :development }.to_sym
   set :root, 'lib/app'
   set :method_override, true
 
