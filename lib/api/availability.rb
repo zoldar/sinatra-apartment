de661479dcfb4bc149013eb77a5ebec6b80d9eb0
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-11-09 16:54:43 +0100

    API call for fetching availability within a given period.

--- lib/api/availability.rb
+++ lib/api/availability.rb
@@ -8,10 +8,21 @@ class LandLordAPI < Sinatra::Base
       halt 403, {error: "Invalid dates given."}.to_json
     end
     
-    if !check
+    unless check
       halt 403, {error: "Apartment is unavailable for the given period."}
     end
 
     status 204
+  end
+  
+  get '/apartment/default/availability/:form/:to' do |from, to|
+    begin
+      availability = Model::Availability.for(Model::Apartment.default).
+        availability(Date.iso8601(from), Date.iso8601(to))
+
+      {availability: availability}.to_json
+    rescue ArgumentError
+      halt 403, {error: "Invalid dates given."}.to_json
+    end
   end
 end
--- test/api/availability_test.rb
+++ test/api/availability_test.rb
@@ -27,14 +27,32 @@ class AvailabilityAppTest < Minitest::Test
     get "/apartment/default/check/2013-02-15/2013-02-19"
     assert_equal 204, last_response.status
   end
-  
+
   def test_check_with_invalid_dates
     get "/apartment/default/check/2013-02-15/2013-02-13"
     assert_equal 403, last_response.status
   end
-  
+
   def test_check_over_the_edge
     get "/apartment/default/check/2013-02-15/2013-02-25"
     assert_equal 403, last_response.status
+  end
+
+  def test_availability_for_a_period
+    expected = {Date.new(2013, 2, 11) => {state: 'unavailable'},
+                Date.new(2013, 2, 12) => {state: 'available'},
+                Date.new(2013, 2, 13) => {state: 'available'},
+                Date.new(2013, 2, 14) => {state: 'available'},
+                Date.new(2013, 2, 15) => {state: 'available'},
+                Date.new(2013, 2, 16) => {state: 'available'},
+                Date.new(2013, 2, 17) => {state: 'available'},
+                Date.new(2013, 2, 18) => {state: 'available'},
+                Date.new(2013, 2, 19) => {state: 'available'},
+                Date.new(2013, 2, 20) => {state: 'available'},
+                Date.new(2013, 2, 21) => {state: 'unavailable'}}
+    
+    get "/apartment/default/availability/2013-02-11/2013-02-21"
+    assert_equal 200, last_response.status
+    assert_equal last_response.body, {availability: expected}.to_json
   end
 end
